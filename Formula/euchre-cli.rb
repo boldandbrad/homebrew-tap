98c4fa079764a0d7a8d0146c79186a03d9a8fd22
Bradley Wojcik <bradleycwojcik@gmail.com>
2024-02-19 14:19:52 -0500

    update euchre-cli version to v0.7.0

--- Formula/euchre-cli.rb
+++ Formula/euchre-cli.rb
@@ -3,20 +3,20 @@ class EuchreCli < Formula
 
   desc "Play euchre in your terminal"
   homepage "https://boldandbrad.github.io/euchre-cli/"
-  url "https://files.pythonhosted.org/packages/c5/52/29e9dd078725c18dfe72327df236915e3c984b54e88eeec30fa6cdbe3a3e/euchre-cli-0.6.1.tar.gz"
-  sha256 "09c863028214ae157a5a6f8cff29283550da22ec909a3dd171d86f4e94bdc206"
+  url "https://files.pythonhosted.org/packages/b6/19/0619bb4868c579b1f9e7197f5f31231da498b4c6b7de4c83e96388393a5d/euchre_cli-0.7.0.tar.gz"
+  sha256 "3e54f9680981769c03cc793da88212de68af2ba73edea4c1025e25fa80e3ce00"
   license "MIT"
 
-  depends_on "python@3.8"
+  depends_on "python3"
 
   resource "click" do
-    url "https://files.pythonhosted.org/packages/f4/09/ad003f1e3428017d1c3da4ccc9547591703ffea548626f47ec74509c5824/click-8.0.3.tar.gz"
-    sha256 "410e932b050f5eed773c4cda94de75971c89cdb3155a72a0831139a79e5ecb5b"
+    url "https://files.pythonhosted.org/packages/96/d3/f04c7bfcf5c1862a2a5b845c6b2b360488cf47af55dfa79c98f6a6bf98b5/click-8.1.7.tar.gz"
+    sha256 "ca9853ad459e787e2192211578cc907e7594e294c7ccc834310722b41b9ca6de"
   end
 
   resource "loguru" do
-    url "https://files.pythonhosted.org/packages/6d/25/0d65383fc7b4f4ce9505d16773b2b2a9f0f465ef00ab337d66afff47594a/loguru-0.5.3.tar.gz"
-    sha256 "b28e72ac7a98be3d28ad28570299a393dfcd32e5e3f6a353dec94675767b6319"
+    url "https://files.pythonhosted.org/packages/9e/30/d87a423766b24db416a46e9335b9602b054a72b96a88a241f2b09b560fa8/loguru-0.7.2.tar.gz"
+    sha256 "e671a53522515f34fd406340ee968cb9ecafbc4b36c679da03c18fd8d0bd51ac"
   end
 
   resource "names" do
@@ -25,6 +25,7 @@ class EuchreCli < Formula
   end
 
   def install
+    # virtualenv_create(libexec, "python3")
     virtualenv_install_with_resources
   end
 
