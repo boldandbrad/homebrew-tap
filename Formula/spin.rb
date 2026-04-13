492d5f50f678c8b39b829e5dea0f8f326e6c06fb
boldandbrad <bradleycwojcik@gmail.com>
2026-04-13 16:51:48 -0400

    feat: add spin patch release v0.1.1 to fix install

--- Formula/spin.rb
+++ Formula/spin.rb
@@ -1,24 +1,28 @@
 class Spin < Formula
   desc "A command line last.fm scrobbler for techies"
   homepage "https://github.com/boldandbrad/spin"
-  version "0.1.0"
+  version "0.1.1"
+
   on_macos do
     on_arm do
-      url "https://github.com/boldandbrad/spin/releases/download/v0.1.0/spin-darwin-arm64.tar.gz"
-      sha256 "db3ad15ece84aa62b8f782fcd9560668a10c4ccee56ff64b4a2fb2b3b173dcee"
+      url "https://github.com/boldandbrad/spin/releases/download/v0.1.1/spin-darwin-arm64.tar.gz"
+      sha256 "05f0887412cb315e7758fa314a01146ea5604d6d8ce6c44d88ce26bc5a61b4a7"
     end
     on_intel do
-      url "https://github.com/boldandbrad/spin/releases/download/v0.1.0/spin-darwin-amd64.tar.gz"
-      sha256 "b24b605c851f15fb9909cbe17dbc2cb96c4d810951a359de39faf891e83a3c19"
+      url "https://github.com/boldandbrad/spin/releases/download/v0.1.1/spin-darwin-amd64.tar.gz"
+      sha256 "0270f545db926ded81031eec615f82f1c95a34f9b91e60d2ff4e0e9befb5ef3a"
     end
   end
+
   on_linux do
-    url "https://github.com/boldandbrad/spin/releases/download/v0.1.0/spin-linux-amd64.tar.gz"
-    sha256 "8c54f2d5cc332f6a79e30b2d5c0239836bcc0ea975bd3717f50f25e7f37462e4"
+    url "https://github.com/boldandbrad/spin/releases/download/v0.1.1/spin-linux-amd64.tar.gz"
+    sha256 "ba4e5f7da999f9f5efcd21b8c7438a43bbf8aa701a4efe3ec885cafb5c2adee1"
   end
+
   def install
     bin.install "spin"
   end
+
   test do
     system "#{bin}/spin", "--version"
   end
